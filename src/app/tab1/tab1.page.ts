47fb6104d2f23412f23bf4011f4b91c2cf5b58a8
Nick Russell <nickynn07@gmail.com>
2020-08-26 20:55:58 +1200

    App Ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  dado1: Number;
+  dado2: Number;
+  dado3: Number;
+  resultado: string;
   constructor() {}
 
+  imagen = ['https://i.giphy.com/media/97e6IX0kayYTK/giphy.webp'];
+
+  random() {
+
+    this.dado1 = Math.floor(Math.random() * 3) + 1;
+    this.dado2 = Math.floor(Math.random() * 3) + 1;
+    this.dado3 = Math.floor(Math.random() * 3) + 1;
+
+    if(this.dado1 === this.dado2 && this.dado2 === this.dado3){
+      this.resultado = 'Ganaste';
+    }
+    else{
+      this.resultado = 'Perdiste'
+    }
+  }
 }
